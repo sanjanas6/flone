6fc504437a2725270398ac78bb7e0ff57bd46879
Sanjana Singh <sanjanasingh7774@gmail.com>
2024-03-11 19:30:04 +0530

    Login added

--- src/pages/other/Login.js
+++ src/pages/other/Login.js
@@ -1,12 +1,51 @@
-import React, { Fragment } from "react";
-import { Link, useLocation } from "react-router-dom"; 
+import React, { Fragment, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom"; 
 import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const LoginRegister = () => {
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
   let { pathname } = useLocation();
+  const navigate = useNavigate();
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
 
+    try {
+        const response = await axios.post('http://localhost:8080/api/v1/auth/login', {
+            phone: phone,
+            password: password
+        });
+        if (response.data) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Registration Successful',
+                text: 'User Registered Successfully!',
+            });
+            navigate("/")
+        } else {
+            // Registration failed
+            Swal.fire({
+                icon: 'error',
+                title: 'Registration Failed',
+                text: response.data.message,
+            });
+        }
+    } catch (error) {
+        // Error during registration
+        Swal.fire({
+            icon: 'error',
+            title: 'Registration Failed',
+            text: 'An error occurred during registration',
+        });
+
+        console.error("Registration failed:", error);
+    }
+};
   return (
     <Fragment>
       <SEO
@@ -28,16 +67,20 @@ const LoginRegister = () => {
                 <div className="login-register-wrapper">
                         <div className="login-form-container">
                           <div className="login-register-form">
-                            <form>
-                              <input
+                            <form onSubmit={handleSubmit}>
+                            <input
                                 type="number"
                                 name="mobile-number"
                                 placeholder="Mobile Number"
+                                value={phone}
+                                onChange={(e)=>setPhone(e.target.value)}
                               />
                               <input
                                 type="password"
                                 name="user-password"
                                 placeholder="Password"
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
                               />
                               <div className="button-box">
                                 <div className="login-toggle-btn">
